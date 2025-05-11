99e391627ef3cb187710efdf669cd1ad714f50ca
Italo Santos <italotsantana75@gmail.com>
2025-05-11 06:23:35 -0300

    Prática Aula 3

---
+++ 1_Nivel_Novato/Aula_3/estruturas_de_decisao_composta.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração das variáveis
+    int num = 31;
+
+
+
+    // Checagem de número par ou ímpar
+    if (num % 2 == 0) {
+        printf("O número é par.");
+    } else {
+        printf("O número é ímpar");
+    }
+
+
+
+    return 0;
+}
