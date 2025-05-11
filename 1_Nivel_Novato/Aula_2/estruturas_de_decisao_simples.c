29c38364f9fa4da6e5104df67a332e084af0aac3
Italo Santos <italotsantana75@gmail.com>
2025-05-11 04:51:24 -0300

    Prática Aula 2

---
+++ 1_Nivel_Novato/Aula_2/estruturas_de_decisao_simples.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração de variáveis
+    int num = 10;
+
+
+
+    // Comparação das variáveis
+    if (num % 2 == 0) {
+        printf("O número é par.\n");
+    }
+
+    printf("Fora da condição IF ELSE.\n");
+
+
+
+    return 0;
+}
